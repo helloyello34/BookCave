187a7c9c2367c92263df768b32d6dcb78bc68c07
helloyello34 <kristinn34@gmail.com>
2018-05-02 13:47:51 +0000

    tussi

--- Models/EntityModels/Book.cs
+++ Models/EntityModels/Book.cs
@@ -1,7 +1,23 @@
+using System;
+
 namespace BookCave.Models.EntityModels
 {
     public class Book
     {
         public int Id { get; set; }
+        public string ISBN { get; set; }
+        public string Language { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int AuthorId { get; set; }
+        public int PageCount { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Rating { get; set; }
+        public uint RatingCount { get; set; }
+        public uint Stock { get; set; }
+        public double price { get; set; }
+        public string Info { get; set; }
+        public double Discount  { get; set; }
     }
 }
