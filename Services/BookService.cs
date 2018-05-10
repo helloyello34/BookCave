d0a28eeca3f15e9dad95d1fad15b3b06722cfd30
osskari <oskarjorgen@gmail.com>
2018-05-10 20:05:01 +0000

    book repo & service and access deined commented

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -14,40 +14,49 @@ namespace BookCave.Services
 
         public BookService()
         {
+            //creates repository layer
             _bookRepo = new BookRepo();
         }
 
         public BookDetailsViewModel GetBookById(int id)
         {
+            //gets book that matches id
             var book = _bookRepo.GetBookById(id);
+            //gets comment for the book
             var bookComments = _bookRepo.GetComments(id);
+            //if the book has comments add them else just return directly
             if( bookComments.Any() ) { book.Comments = bookComments; }
 
             return book;
         }
         public List<BookTableViewModel> GetBooksByGenre(int order)
         {
+            //gets books of all genres and returns them
             var books = _bookRepo.GetBooksByGenre(order);
             return books;
         }
         public List<BookTableViewModel> GetBooksByGenre(string selectedGenre, int order)
         {
-           var books = _bookRepo.GetBooksByGenre(selectedGenre, order);
-           return books;
+            //gets books of a specific genre and returns them
+            var books = _bookRepo.GetBooksByGenre(selectedGenre, order);
+            return books;
         }
         public BookFrontPageViewModel GetFrontPageBooks()
         {
+            //gets front page books and returns them
             var books = _bookRepo.GetFrontPageBooks();
             return books;
         }
         public BookTopTenViewModel GetTopTenBooks()
         {
+            //gets 10 top rated books and returns them
             var books = _bookRepo.GetTopTenBooks();
 
             return books;
         }
         public void AddBook(BookInputModel bookInputModel)
         {
+            //returns new book
             var book = new Book
             {
                 ISBN = bookInputModel.ISBN,
@@ -71,16 +80,19 @@ namespace BookCave.Services
 
         public void AddAuthor(Author author)
         {
+            //adds new author
             _bookRepo.AddAuthor(author);
         }
         
         public List<AuthorsViewModel> GetAuthorList()
         {
+            //get author list from database
             return _bookRepo.GetAuthorList();
         }
 
         public void AddComment (int id, CommentInputModel comment)
         {
+            //adds data into new comment
             var newComment = new Comment {
                 BookId = id,
                 Comments = comment.Comment,
@@ -91,6 +103,7 @@ namespace BookCave.Services
         }
         public void UpdateBookRating(int id, int rating)
         {
+            //updates the overall rating of the book with the new rating
             var book = _bookRepo.GetBookEntity(id);
             var currentRatingSum = book.Rating * book.RatingCount;
             book.RatingCount++;
@@ -99,6 +112,7 @@ namespace BookCave.Services
         }
         public List<string> GetGenresList()
         {
+            //gets a list of genres
             var genresList = _bookRepo.GetGenresList();
 
             return genresList;
@@ -106,6 +120,7 @@ namespace BookCave.Services
 
         public List<BookTableViewModel> findBooks(string searchString, int order)
         {
+            //gets a list of books that fit the search string
             var selectedBooks = _bookRepo.findBooks(searchString, order);
 
             return selectedBooks;
